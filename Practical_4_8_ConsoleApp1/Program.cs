1da8f96531650ce76138b6ef83394d0ec6b5228b
User <genik-vlad@mail.ru>
2022-07-03 16:01:46 +0300

    Добавьте файлы проекта.

---
+++ Practical_4_8_ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practical_4_8_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int summ=0;
+            Console.Write(" Введите количество строк :");
+            int line = int.Parse(Console.ReadLine());
+            Console.Write(" Введите количество столбцов :");
+            int column = int.Parse(Console.ReadLine());
+            int[,] array = new int[line, column];
+            Random rand = new Random();
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    array[i, j] = rand.Next(100);
+                    Console.Write($" {array[i , j]}");
+                    summ=summ+array[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" Сумма всех элементов : {0}",summ);
+            Console.ReadLine();
+        }
+    }
+}
